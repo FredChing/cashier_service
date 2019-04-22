571a2aaa27bdee2fbcc4ae7c6d1120d7d9c9293a
chengfei <chengfei@wdwd.com>
2019-04-22 12:12:23 +0800

    请求类型 修改为 get

--- routers/router.go
+++ routers/router.go
@@ -7,7 +7,7 @@ import (
 
 func init() {
 	beego.Router("/checkout/pay/alipay", &controllers.Alipay{}, "post:Pay") //支付宝页面扫码支付
-	beego.Router("/checkout/pay/alipay_wap", &controllers.AlipayWap{}, "post:Pay") //支付宝手机网站支付
+	beego.Router("/checkout/pay/alipay_wap", &controllers.AlipayWap{}, "get:Pay") //支付宝手机网站支付
 	//beego.Router("/checkout/payback/alipay/return_url/:callback", &payback_ctrl.Alipay{}, "*:HandleReturn")
 	//beego.Router("/checkout/payback/alipay/notify_url/:callback/:sms", &payback_ctrl.Alipay{}, "*:HandleNotify")
 }
